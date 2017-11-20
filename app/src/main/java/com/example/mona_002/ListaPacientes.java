e60fd702b295d5a1ae6bffe49a5d376b6d773218
Ronnypetson <rsronnypetson4@gmail.com>
2017-11-19 23:51:36 -0300

    update

--- app/src/main/java/com/example/mona_002/ListaPacientes.java
+++ app/src/main/java/com/example/mona_002/ListaPacientes.java
@@ -1,7 +1,10 @@
 package com.example.mona_002;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaPacientes extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class ListaPacientes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacientes);
+
+        //Intent intent = getIntent();
+        // Using ArrayAdapter
+        String[] StringArray = {"1","2","3"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_lista_pacientes,StringArray);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/mona_002/TelaLogin.java
+++ app/src/main/java/com/example/mona_002/TelaLogin.java
@@ -1,7 +1,9 @@
 package com.example.mona_002;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TelaLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+    }
+
+    public void entrar(View view){
+        Intent intent = new Intent(this, ListaPacientes.class);
+        startActivity(intent);
+        this.finish();
     }
 }
